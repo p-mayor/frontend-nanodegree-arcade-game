f8f11c09eee6053b18d1f52c2f8c18a5d3e42531
Tamburkey <petem14@gmail.com>
2017-10-05 02:34:53 -0400

    created player class

--- js/app.js
+++ js/app.js
@@ -25,6 +25,10 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function () {
+    this.sprite = 'images/char-boy.png';
+}
+
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
